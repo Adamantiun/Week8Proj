5712b19809644adfc354f830373cbfb4df4183c0
Adamantiun <40891416+Adamantiun@users.noreply.github.com>
2021-12-10 15:08:03 +0000

    Ex1 finished

--- src/main/java/pt/up/fe/ldts/example1/OrderLine.java
+++ src/main/java/pt/up/fe/ldts/example1/OrderLine.java
@@ -1,11 +1,27 @@
 package pt.up.fe.ldts.example1;
 
 public class OrderLine {
-    public Product product;
-    public int quantity;
+    private Product product;
+    private int quantity;
 
     public OrderLine(Product product, int quantity) {
         this.product = product;
         this.quantity = quantity;
+    }
+
+    public Product getProduct(){ return product; }
+
+    void setProduct(Product product){ this.product = product; }
+
+    public int getQuantity(){ return quantity; }
+
+    void setQuantity(int quantity){ this.quantity = quantity; }
+
+    public double getTotal() {
+        return product.getPrice() * quantity;
+    }
+
+    public void printOrder(StringBuffer printBuffer) {
+        printBuffer.append(product.getName() + "(x" + quantity + "): " + (product.getPrice() * quantity) + "\n");
     }
 }
