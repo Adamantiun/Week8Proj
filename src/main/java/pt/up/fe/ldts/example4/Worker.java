b5f2d7eeaebebfcc602305662a2b7e14adc07b06
Adamantiun <40891416+Adamantiun@users.noreply.github.com>
2021-12-21 00:32:45 +0000

    4 done

--- src/main/java/pt/up/fe/ldts/example4/Worker.java
+++ src/main/java/pt/up/fe/ldts/example4/Worker.java
@@ -2,15 +2,13 @@ package pt.up.fe.ldts.example4;
 
 import java.util.Objects;
 
-public class Worker {
-    private final String name;
-    private final String phone;
+public class Worker extends Human {
+
     private final String username;
     private final String password;
 
     public Worker(String name, String phone, String username, String password) {
-        this.name = name;
-        this.phone = phone;
+        super(name, phone);
         this.username = username;
         this.password = password;
     }
@@ -18,28 +16,28 @@ public class Worker {
     public boolean login(String username, String password) {
         return this.username.equals(username) && this.password.equals(password);
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
     @Override
+    public String getName() {
+        return super.getName();
+    }
+    @Override
+    public String getPhone() {
+        return super.getPhone();
+    }
+    @Override
+    @SuppressWarnings("EqualsGetClass")
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Worker worker = (Worker) o;
-        return Objects.equals(name, worker.name) &&
-                Objects.equals(phone, worker.phone) &&
+        return Objects.equals(super.getName(), worker.getName()) &&
+                Objects.equals(super.getPhone(), worker.getPhone()) &&
                 Objects.equals(username, worker.username) &&
                 Objects.equals(password, worker.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, phone, username, password);
+        return Objects.hash(super.getName(), super.getPhone(), username, password);
     }
 }
